3dbaae411e8ee39767d1eaf07621f87f3afe2e79
Philip Yu <pyu3@mail.sfsu.edu>
2019-07-02 10:43:10 -0700

    Setup LocationManager to get GPS Data from iPhone

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -7,33 +7,31 @@
 //
 
 import UIKit
+import CoreLocation
 
-
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
     let APP_ID = "***REMOVED***"
     /***Get your own App ID at https://openweathermap.org/appid ****/
     
-
     //TODO: Declare instance variables here
-    
+    let locationManager = CLLocationManager()
 
-    
     //Pre-linked IBOutlets
     @IBOutlet weak var weatherIcon: UIImageView!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
-
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
-        
         //TODO:Set up the location manager here.
-    
-        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
         
     }
     
